d1029e653687b00b43d29851621088ded2705f1e
palamar <ss380982599941@gmail.com>
2022-02-20 14:32:31 +0200

    updates

--- src/app/modules/main/dashboard/photo-board/photo-board.component.ts
+++ src/app/modules/main/dashboard/photo-board/photo-board.component.ts
@@ -15,8 +15,8 @@ const PHOTOS_DATA: BoardPhoto[] = [
   {name: 'slide.jpg', path: '/assets/gallery/slide2.jpg', position: {x: 352, y: 187}, transform: -18},
   {name: 'slide2.jpg', path: '/assets/gallery/slide2.jpg', position: {x: 384, y: 386}, transform: 10},
   {name: 'slide3.jpg', path: '/assets/gallery/slide3.jpg', position: {x: -400, y: 400}, transform: -6},
-  {name: 'slide3.jpg', path: '/assets/gallery/slide3.jpg', position: {x: -100, y: 100}, transform: -2},
-  {name: 'slide2.jpg', path: '/assets/gallery/slide.jpg', position: {x: 580, y: 200}, transform: 10},
+  {name: 'slide3.jpg', path: '/assets/gallery/slide3.jpg', position: {x: -100, y: 50}, transform: -2},
+  {name: 'slide2.jpg', path: '/assets/gallery/slide.jpg', position: {x: -190, y: 200}, transform: 10},
 ];
 
 @Component({
